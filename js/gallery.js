7e00c9a978b6b7befcc7739c49a166f420263be6
Oleksandr-Kalnaus <oleksandrkalnaus@gmail.com>
2024-07-23 11:37:56 +0100

    Edit before start

---
+++ js/gallery.js
@@ -0,0 +1,106 @@
+const images = [
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+        description: 'Hokkaido Flower',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+        description: 'Container Haulage Freight',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+        description: 'Aerial Beach View',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+        description: 'Flower Blooms',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+        description: 'Alpine Mountains',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+        description: 'Mountain Lake Sailing',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+        description: 'Alpine Spring Meadows',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+        description: 'Nature Landscape',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+        description: 'Lighthouse Coast Sea',
+    },
+];
+
+const createGalleryCard = pictureInfo => {
+    return `<li class="gallery-item">
+            <a class="gallery-link" href="${pictureInfo.original}">
+                <img
+                class="gallery-image"
+                src="${pictureInfo.preview}"
+                data-source="${pictureInfo.original}"
+                alt="${pictureInfo.description}"
+                />
+            </a>
+        </li>`
+};
+
+const galleryCardsTemplate = images.map(imgInfo => createGalleryCard(imgInfo)).join(``);
+
+const imageGallery = document.querySelector(`.gallery`);
+// якщо додаємо з нуля 
+imageGallery.innerHTML = galleryCardsTemplate;
+// якщо додаємо до створеного списку
+// imageGallery.insertAdjacentHTML(`beforeend`, galleryCardsTemplate);
+
+// додаємо слухача на батьківський елемент і функцію як буде виконуватися
+imageGallery.addEventListener("click", );
+
+
+// function selectColor(event) {
+//   if (event.target.nodeName !== "BUTTON") {
+//     return; // користувач клікнув між кнопками
+//   }
+
+//   const selectedColor = event.target.dataset.color; // користувач клікнув на кнопку і ми маємо доступ до її атрибутів
+// }
+
+
+
+    
+
+
+
+
--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const nameInput = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-const defaultOutput = output.textContent;
-
-nameInput.addEventListener("input", (event) => {
-    const inputValue = event.currentTarget.value.trim();
-    output.textContent = inputValue ? inputValue : defaultOutput;
-});
